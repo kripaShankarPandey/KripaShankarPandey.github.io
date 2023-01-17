15f2b3eca51ee39236cb5306e4012ae542428513
kripa Shankar Pandey <kripapandey302@gmail.com>
2023-01-17 19:50:04 +0530

    res done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import Contact from "./Contact";
 import Projects from "./Projects";
 import Skills from "./Skills";
 import GitHubCalendar from "react-github-calendar";
-import { Box, Heading, VStack } from "@chakra-ui/react";
+import { Box, Heading, Image, VStack } from "@chakra-ui/react";
 const Home = () => {
   return (
     <div id="Home">
@@ -22,11 +22,20 @@ const Home = () => {
         <GitHubCalendar username="kripashankarpandey" />
       </VStack>
 
-      {/* <VStack>
-        <Box>
-          https://github-readme-stats.vercel.app/api?username=Omchand27&show_icons=true&include_all_commits=true&count_private=true&hide=issues,contribs&border_radius=20&locale=en&theme=radical
-        </Box>
-      </VStack> */}
+      <VStack>
+        <Image
+          src="
+          https://github-readme-stats.vercel.app/api?username=kripaShankarPandey&show_icons=true&include_all_commits=true&count_private=true&hide=issues,contribs&border_radius=20&locale=en&theme=radical"
+          alt="github status"
+        />
+      </VStack>
+      <VStack mt={10}>
+        <Image
+          src="
+          https://github-readme-stats.vercel.app/api/top-langs/?username=kripaShankarPandey&layout=compact&hide=Shell&border_radius=20&theme=radical"
+          alt="github status"
+        />
+      </VStack>
       <Contact />
       {/* https://github-readme-stats.vercel.app/api?username=Omchand27&show_icons=true&include_all_commits=true&count_private=true&hide=issues,contribs&border_radius=20&locale=en&theme=radical */}
       {/* https://github-readme-stats.vercel.app/api/top-langs/?username=Omchand27&layout=compact&hide=Shell&border_radius=20&theme=radical */}
