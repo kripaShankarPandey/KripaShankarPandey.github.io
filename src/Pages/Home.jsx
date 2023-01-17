06722e31ea7650e1bb70d2aea90f1f1adc0fe42e
kripa Shankar Pandey <kripapandey302@gmail.com>
2023-01-17 17:26:08 +0530

    ol

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -20,11 +20,11 @@ const Home = () => {
         <GitHubCalendar username="kripashankarpandey" />
       </VStack>
 
-      <VStack>
+      {/* <VStack>
         <Box>
           https://github-readme-stats.vercel.app/api?username=Omchand27&show_icons=true&include_all_commits=true&count_private=true&hide=issues,contribs&border_radius=20&locale=en&theme=radical
         </Box>
-      </VStack>
+      </VStack> */}
       <Contact />
       {/* https://github-readme-stats.vercel.app/api?username=Omchand27&show_icons=true&include_all_commits=true&count_private=true&hide=issues,contribs&border_radius=20&locale=en&theme=radical */}
       {/* https://github-readme-stats.vercel.app/api/top-langs/?username=Omchand27&layout=compact&hide=Shell&border_radius=20&theme=radical */}
