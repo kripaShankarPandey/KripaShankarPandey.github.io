e6a959f685a333d0bfee279d951eba26fae74c1f
kripa Shankar Pandey <kripapandey302@gmail.com>
2023-05-21 22:04:10 +0530

    All Link fixed

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -23,160 +23,90 @@ import { BsGithub, BsTwitter, BsLinkedin, BsPerson } from "react-icons/bs";
 
 export default function Contact() {
   return (
-    <Container id="contact" maxW="full" mt={0} centerContent overflow="hidden">
-      <Flex>
-        <Box
-          bg="teal"
-          color="white"
-          borderRadius="lg"
-          m={{ sm: 4, md: 16, lg: 10 }}
-          p={{ sm: 5, md: 5, lg: 16 }}
-        >
-          <Box p={4}>
-            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
-              <WrapItem>
-                <Box>
-                  <Heading> Contact me</Heading>
-                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
-                    <VStack pl={0} spacing={3} alignItems="flex-start">
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdPhone color="#ffffff" size="20px" />}
-                        id="contact-phone"
-                      >
-                        +91-9450341634
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="230px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdEmail color="#ffffff" size="20px" />}
-                        id="contact-email"
-                      >
-                        kripapandey302@gmail.com
-                      </Button>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdLocationOn color="#ffffff" size="20px" />}
-                      >
-                        Basti, U.P India
-                      </Button>
-                    </VStack>
-                  </Box>
-                  <HStack
-                    mt={{ lg: 10, md: 10 }}
-                    spacing={5}
-                    px={5}
-                    alignItems="flex-start"
-                  >
-                    <Link href="https://twitter.com/kirpa_pandey">
-                      <IconButton
-                        aria-label="twitter"
-                        variant="ghost"
-                        size="lg"
-                        isRound={true}
-                        _hover={{ bg: "#0D74FF" }}
-                        icon={<BsTwitter size="28px" />}
-                      />
-                    </Link>
-                    <Link
-                      href="https://github.com/kripaShankarPandey"
-                      id="contact-github"
-                    >
-                      <IconButton
-                        aria-label="github"
-                        variant="ghost"
-                        size="lg"
-                        isRound={true}
-                        _hover={{ bg: "#0D74FF" }}
-                        icon={<BsGithub size="28px" />}
-                      />
-                    </Link>
-                    <Link
-                      href="https://www.linkedin.com/in/kripa-pandey-9465b215a"
-                      id="contact-linkedin"
-                    >
-                      <IconButton
-                        aria-label="github"
-                        variant="ghost"
-                        size="lg"
-                        isRound={true}
-                        _hover={{ bg: "#0D74FF" }}
-                        icon={<BsLinkedin size="28px" />}
-                      />
-                    </Link>
-                  </HStack>
-                </Box>
-              </WrapItem>
-              <WrapItem>
-                <Box bg="white" borderRadius="lg">
-                  <Box m={8} color="#0B0E3F">
-                    <VStack spacing={5}>
-                      <FormControl
-                        action="https://getform.io/f/1b665fab-8147-4dab-80f1-2a00279ff6bd"
-                        method="POST"
-                        id="name"
-                      >
-                        <FormLabel>Your Name</FormLabel>
-                        <InputGroup borderColor="#E0E1E7">
-                          <InputLeftElement
-                            pointerEvents="none"
-                            children={<BsPerson color="gray.800" />}
-                          />
-                          <Input type="text" size="md" />
-                        </InputGroup>
-                      </FormControl>
-                      <FormControl id="name">
-                        <FormLabel>Mail</FormLabel>
-                        <InputGroup borderColor="#E0E1E7">
-                          <InputLeftElement
-                            pointerEvents="none"
-                            children={<MdOutlineEmail color="gray.800" />}
-                          />
-                          <Input type="text" size="md" />
-                        </InputGroup>
-                      </FormControl>
-                      <FormControl id="name">
-                        <FormLabel>Message</FormLabel>
-                        <Textarea
-                          borderColor="gray.300"
-                          _hover={{
-                            borderRadius: "gray.300",
-                          }}
-                          placeholder="message"
-                        />
-                      </FormControl>
-                      <FormControl id="name" float="right">
-                        <Button
-                          variant="solid"
-                          bg="teal"
-                          color="white"
-                          _hover={{}}
-                        >
-                          Send Message
-                        </Button>
-                      </FormControl>
-                    </VStack>
-                  </Box>
-                </Box>
-              </WrapItem>
-            </Wrap>
+    <Box bg="teal" color="white">
+      <Box p={4}>
+        <Box>
+          <Heading> Keep in touch</Heading>
+          <Box>
+            <Box>
+              <Button
+                size="md"
+                height="48px"
+                width="200px"
+                variant="ghost"
+                m="3"
+                color="#DCE2FF"
+                _hover={{ border: "2px solid #1C6FEB" }}
+                leftIcon={<MdPhone color="#ffffff" size="20px" />}
+                id="contact-phone">
+                +91-9450341634
+              </Button>
+              <Button
+                size="md"
+                height="48px"
+                width="230px"
+                variant="ghost"
+                m="3"
+                color="#DCE2FF"
+                _hover={{ border: "2px solid #1C6FEB" }}
+                leftIcon={<MdEmail color="#ffffff" size="20px" />}
+                id="contact-email">
+                kripapandey302@gmail.com
+              </Button>
+              <Button
+                size="md"
+                height="48px"
+                width="200px"
+                variant="ghost"
+                m="3"
+                color="#DCE2FF"
+                _hover={{ border: "2px solid #1C6FEB" }}
+                leftIcon={<MdLocationOn color="#ffffff" size="20px" />}>
+                Basti, U.P India
+              </Button>
+            </Box>
+          </Box>
+          <Box>
+            <Link href="https://twitter.com/kirpa_pandey">
+              <IconButton
+                aria-label="twitter"
+                variant="ghost"
+                m="3"
+                size="lg"
+                isRound={true}
+                _hover={{ bg: "#0D74FF" }}
+                icon={<BsTwitter size="28px" />}
+              />
+            </Link>
+            <Link
+              href="https://github.com/kripaShankarPandey"
+              id="contact-github">
+              <IconButton
+                aria-label="github"
+                variant="ghost"
+                m="3"
+                size="lg"
+                isRound={true}
+                _hover={{ bg: "#0D74FF" }}
+                icon={<BsGithub size="28px" />}
+              />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/kripa-pandey-9465b215a"
+              id="contact-linkedin">
+              <IconButton
+                aria-label="github"
+                variant="ghost"
+                m="3"
+                size="lg"
+                isRound={true}
+                _hover={{ bg: "#0D74FF" }}
+                icon={<BsLinkedin size="28px" />}
+              />
+            </Link>
           </Box>
         </Box>
-      </Flex>
-    </Container>
+      </Box>
+    </Box>
   );
 }
